a39eeee84381440e7b33822bb26379aa5e699ecf
Baccatore <yuichiro.suga.5@gmail.com>
2018-05-09 20:24:49 +0900

    Update B matrix

--- mod.py
+++ mod.py
@@ -8,12 +8,14 @@
 # smaller files
 #************************************************************
 import itertools
+import numpy.linalg as LA
 
 import config as cfg
 
 
 def simple_product(i,j):
     return itertools(range(i), range(j))
+
 
 def kron_delta(i, j):
     if i == j:
@@ -22,6 +24,11 @@ def kron_delta(i, j):
         return 0
 
 
+def zero_matrix(i,j):
+    a = np.zeros([i,j],dtype=np.float128)
+    a = np.matrix(a)
+    return a
+    
 #Shape function
 # x @ Position vector as argument
 # p @ Position vector of node1
@@ -78,53 +85,52 @@ def get_N(X):
 # This function varies depending on the number of node
 # which consits of one element for each model
 def b_matrix(i_element, node, x, e):
-    #Four times precision is better
-    #XXX Why?
-    b_m = np.zeros([3,8], dtype=np.float128)
+    b_m = zero_matrix(3,8)
+
     #Nodal index which consist of the element
-    i_n = np.zeros([4], dytpe=np.int8)
-    i_n[0] = node[:,i_element] -1
-    print(ni) #XXX Validation if substitution is successed
-              #XXX Delete here once checked
-    #dN/da,dN/db
-    #TODO Abstractize to adopt it for higher dimension
-    dN1_da = -0.25*(1.0-b)
-    dN2_da =  0.25*(1.0-b)
-    dN3_da =  0.25*(1.0+b)
-    dN4_da = -0.25*(1.0+b)
-    dN1_db = -0.25*(1.0-a)
-    dN2_db = -0.25*(1.0+a)
-    dN3_db =  0.25*(1.0+a)
-    dN4_db =  0.25*(1.0-a)
-    #Jaconbi matrix and det(J)
-    j_m = np.zeros([2,2], dtype=np.float128)
+    i, j, k, l = node[:,i_element] -1
+    xi  = zero_matrix(2,4)
+    xi  = np.matri([x[:,i], x[:,j], x[:,k], x[:,l]]) 
+    print(xi)
+
+    #dN/da, dN/db
+    dNi_dai = zero_matrix(2,4)
+    #dNi_db = np.zeros([4], dtype=np.float128)
+    dNi_dai[0,0] = -0.25*(1.0-b)
+    dNi_dai[0,1] =  0.25*(1.0-b)
+    dNi_dai[0,2] =  0.25*(1.0+b)
+    dNi_dai[0,3] = -0.25*(1.0+b)
+    dNi_dai[1,0] = -0.25*(1.0-a)
+    dNi_dai[1,1] = -0.25*(1.0+a)
+    dNi_dai[1,2] =  0.25*(1.0+a)
+    dNi_dai[1,3] =  0.25*(1.0-a)
+    
+    #Inverse of Jaconbi matrix 
+    j_m = zero_matirx(2,2)
     #FIXME Can be written more more easily?
     #Write down elements, can be ortho production
-    for i in range(2)
-        j_m[0,i] = dn1a*x[i,i]+dn2a*x[i,j]+dn3a*x[i,k]+dn4a*x[i,l] 
-        j_m[1,i] = dn1b*x[i,i]+dn2b*x[i,j]+dn3b*x[i,k]+dn4b*x[i,l]
-    detJ = np.det(J)
+    #for i in range(4): #Nb of node in one element
+    #    j_m[0,0] += dNi_dai[0,i]*x[0,i]
+    #    j_m[0,1] += dNi_dai[0,i]*x[1,i]
+    #    j_m[1,0] += dNi_dai[1,i]*x[0,i]
+    #    j_m[1,1] += dNi_dai[1.i]*x[1,i]
+    j_m = dNi_dai * x.T
+    # inverse J
+    j_m = j_m.I
     print(j_m)   #XXX Validation
-    print(detJ)  #XXX Validation
+    
+    #[dN/dx] = [J] * [dN/da]
+    dNi_dxi = zero_matrix(2,4)
+    dNi_dxi = j_m * dNi_dai
+
     #[B]=[dN/dx][dN/dy]
     #Can be written shorter with NumPy!
     #for i,j in simple_product(2, 4):
-    bm[0,0]= J22*dn1a-J12*dn1b
-    bm[0,2]= J22*dn2a-J12*dn2b
-    bm[0,4]= J22*dn3a-J12*dn3b
-    bm[0,6]= J22*dn4a-J12*dn4b
-    bm[1,1]=-J21*dn1a+J11*dn1b
-    bm[1,3]=-J21*dn2a+J11*dn2b
-    bm[1,5]=-J21*dn3a+J11*dn3b
-    bm[1,7]=-J21*dn4a+J11*dn4b
-    bm[2,0]=-J21*dn1a+J11*dn1b
-    bm[2,1]= J22*dn1a-J12*dn1b
-    bm[2,2]=-J21*dn2a+J11*dn2b
-    bm[2,3]= J22*dn2a-J12*dn2b
-    bm[2,4]=-J21*dn3a+J11*dn3b
-    bm[2,5]= J22*dn3a-J12*dn3b
-    bm[2,6]=-J21*dn4a+J11*dn4b
-    bm[2,7]= J22*dn4a-J12*dn4b
+    for i in range(4)
+        bm[0,i*2  ]= dNi_dxi[0,i    ] 
+        bm[1,i*2+1]= dNi_dxi[1,i*2+1] 
+        bm[2,i*2  ]= dNi_dxi[1,i    ]
+        bm[2,i*2+1]= dNi_dxi[0,i*2+1]
     return b_m
 
 
